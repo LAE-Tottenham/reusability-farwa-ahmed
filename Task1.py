81184115019465daba7ac359e0e9948847a68bc5
farwa-ahmed <Farwa.Ahmed@laetottenham.ac.uk>
2024-10-06 23:02:43 +0100

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,6 +1,6 @@
 # Help! My code is too messy :( Please help me organise it and extract out the duplications.
 
-# Define your reusable functions here:
+# Define your reusable functions here: input_opp1 , input_adj1 , find_hyp1 , input_opp2 , input_adj2 , find_hyp2 , answer
 # Make sure each function only does ONE thing!!!!!!!!!!!
 
 
@@ -8,7 +8,13 @@
 ###########################################
 
 def input_opp1():
-    opp1 = float(input("Enter your first triangle's opposite side length: "))
+    opp1 = input("Enter your first triangle's opposite side length: ")
+    z=False
+    while z==False:
+        if opp1==float:
+            z=True:
+        else:
+            print("invalid. try again") 
     return(opp1)
 def input_adj1():
     adj1 = float(input("Enter your first triangle's adjacent side length: "))
@@ -20,7 +26,7 @@ import math
 def find_hyp1():
     hyp1 = math.sqrt(opp1**2 + adj1**2)
     return(hyp1)
-
+hyp1=find_hyp1()
 
 def input_opp2():
     opp2 = float(input("Enter your second triangle's opposite side length: "))
@@ -36,20 +42,21 @@ def find_hyp2():
     return(hyp2)
 hyp2=find_hyp2()    
 
+ def answer():
+     hyp3 = math.sqrt(hyp1**2 + hyp2**2)
+     return hyp3
 
-    import math
-    hyp3 = math.sqrt(hyp1**2 + hyp2**2)
-    return hyp3
-
-weird_answer = weird_calculation()
-print(weird_answer)
-
+x=answer()
 
 # After you have written the reusable functions, answer the following:
 # Questions:
 # 1. What are the preconditions for your code not to break?
+#         the user must enter numbers and not letters, symbols or words
 # 2. Validate the user's input based on your preconditions.
-# 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise.
+#          i made sure that the user can only input a float in the first function
+# 3. Why was it useful to use reusable components in this case? Please mention at least 2 reasons and don't forget to contextualise
+#          when we need to repeat one specific step (like when the answer is worng because of invalid input) we dont need to repeat alot of steps in one function
+#          the code is more organised and easier to understand and spot eroors for example finding adj1 instead of adj2
 
 # Further Tasks:
 # 1. Put your functions in seperate appropriate files and import them in.
